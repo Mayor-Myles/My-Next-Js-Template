e3024b43c465dd696d97993c179f55bd686d6272
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2025-12-14 19:12:30 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
 import { ChakraProvider } from '@chakra-ui/react';
 
+import { RecoilRoot } from 'recoil';
+
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
       <body>
+        <RecoilRoot>
         <ChakraProvider>{children}</ChakraProvider>
+          </RecoilRoot>
       </body>
     </html>
   );
