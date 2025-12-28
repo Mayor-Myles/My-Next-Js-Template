fab7e54fac79c224df98f68c25890a93d008b867
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2025-12-28 08:58:36 +0100

    Change grid columns based on mobile view

--- adverts.tsx
+++ adverts.tsx
@@ -43,7 +43,7 @@ export default function Adverts() {
       >
       
     <SimpleGrid 
-      columns={isMobile ? 1: 3} 
+      columns={isMobile ? 2 : 3} 
       spacing={4} 
       p={4}
       maxW={isMobile ? "100%" : "600px"}
