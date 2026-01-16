5e58eca7431c8d9445510c39139b50d67577f995
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-01-16 16:23:12 +0100

    Update wallet.tsx

--- wallet.tsx
+++ wallet.tsx
@@ -11,7 +11,7 @@ export default function Wallet() {
   
   return (
 
-<Flex mt="4" mx={{base:"1em", md:"8em", lg:"14em", xl:"18em" }} justifyContent="center">
+<Flex bg="black" mt="4" mx={{base:"1em", md:"8em", lg:"14em", xl:"18em" }} justifyContent="center">
      
     <Box
       h="150px"
