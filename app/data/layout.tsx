b630e63d7bd8067e80bf5bb4ef2c38d6594d0b79
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-01-23 23:54:40 +0100

    Add RecoilRoot to RootLayout component

--- app/data/layout.tsx
+++ app/data/layout.tsx
@@ -1,1 +1,14 @@
 
+"use client";
+
+import { RecoilRoot } from "recoil";
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>
+        <RecoilRoot>{children}</RecoilRoot>
+      </body>
+    </html>
+  );
+}
