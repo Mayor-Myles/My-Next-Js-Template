6193674b26dbc7b15b1faf7bb4fe6140fe993d14
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-01-24 10:39:15 +0100

    Update layout.tsx

--- app/airtime/layout.tsx
+++ app/airtime/layout.tsx
@@ -1,1 +1,23 @@
 
+// app/layout.tsx
+'use client';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import Spinner from '@/spinner';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <ChakraProvider>
+          <Spinner />
+          {children}
+        </ChakraProvider>
+      </body>
+    </html>
+  );
+}
