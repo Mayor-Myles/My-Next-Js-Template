68e7020b9c4ee88c57ecc05c367c21e8094d48f2
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-01-24 16:26:48 +0100

    Update topnavbar.jsx

--- topnavbar.jsx
+++ topnavbar.jsx
@@ -83,17 +83,17 @@ const router = useRouter();
             <Box p={2} border="2px solid gray">
             <Avatar
       name={user.name}
-      src={user.gender === "male"? "https://i.pinimg.com/564x/b3/e5/db/b3e5db5a3bf1399f74500a6209462794.jpg" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXB_1EHdjgjcvq0AK52i0YlenrCIctpJJ74A&usqp=CAU" }
+      src={user.gender === "male" ? "https://i.pinimg.com/564x/b3/e5/db/b3e5db5a3bf1399f74500a6209462794.jpg" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXB_1EHdjgjcvq0AK52i0YlenrCIctpJJ74A&usqp=CAU" }
       size="md"
     />
             </Box>      
               ) :
            (
-              
+             <>
            <IconButton size="lg" onClick={toggleColorMode} icon={colorMode ==="light" ? <CiDark size="1.7em" /> : <CiLight size="1.7em" />} />
             
             <Button
-              onClick={()=>{router.push("/login")}}
+              onClick={()=>router.push("/login")}
               size="sm"
               variant="outline"
               borderColor="purple.600"
@@ -111,6 +111,7 @@ const router = useRouter();
             >
               Get Started
             </Button>
+             </>
               )
 
               }
