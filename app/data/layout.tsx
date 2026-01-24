3a4d37326a71ba71777a975a3235363b1940b95c
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-01-24 09:27:38 +0100

    Refactor RootLayout to remove ChakraProvider
    Removed ChakraProvider and Spinner components from RootLayout.

--- app/data/layout.tsx
+++ app/data/layout.tsx
@@ -1,25 +1,11 @@
 
 "use client";
 
-import { ChakraProvider } from '@chakra-ui/react';
-import Spinner from "@/spinner";
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-
-      <body>
-        
-        <ChakraProvider>
-
-          <Spinner />
-          
-          {children}
-        
-        </ChakraProvider>
-          
-      </body>
-    </html>
+    
+    <></>
   );
 }
