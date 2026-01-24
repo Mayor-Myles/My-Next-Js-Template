95f5fb75b18463ab06d4f9fda5ebd20b52321d8b
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-01-24 07:17:09 +0100

    Remove RecoilRoot from RootLayout
    Removed RecoilRoot wrapper from RootLayout component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@
 
 import { ChakraProvider } from '@chakra-ui/react';
 
-import { RecoilRoot } from 'recoil';
-
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -11,9 +9,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
       <body>
-        <RecoilRoot>
+        
         <ChakraProvider>{children}</ChakraProvider>
-          </RecoilRoot>
+          
       </body>
     </html>
   );
