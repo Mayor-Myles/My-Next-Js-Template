c50a3d8db93915a038766c405ce1831e61b92172
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-01-24 16:44:56 +0100

    Update topnavbar.jsx

--- topnavbar.jsx
+++ topnavbar.jsx
@@ -80,7 +80,7 @@ const router = useRouter();
     {
           user.isAuthenticated ? (
 <>
-            <Box borderRadius="full" m={2} p={2} border="2px solid "{borderColor}>
+            <Box borderRadius="full" m={2} p={2} border={`2px solid ${borderColor} `}>
             <Avatar
       name={user.name}
       src={user.gender === "male" ? "https://i.pinimg.com/564x/b3/e5/db/b3e5db5a3bf1399f74500a6209462794.jpg" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXB_1EHdjgjcvq0AK52i0YlenrCIctpJJ74A&usqp=CAU" }
